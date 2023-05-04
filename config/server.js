931203e946ebbd221fd004bc6e5873340e8de331
Rodrigo Alejandro Juarez <rodrigo.juarez.inf@gmail.com>
2023-05-04 08:57:27 -0300

    Adding public URL for admin too

--- config/server.js
+++ config/server.js
@@ -1,11 +1,18 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET", "secret"),
+    },
+    serveAdminPanel: env.bool("SERVE_ADMIN", true),
+    url: env("PUBLIC_URL", "https://memo-test-challenge-strapi.onrender.com/"),
   },
-  url: env('PUBLIC_URL', 'https://memo-test-challenge-strapi.onrender.com/'),
+  app: {
+    keys: env.array("APP_KEYS"),
+  },
+  url: env("PUBLIC_URL", "https://memo-test-challenge-strapi.onrender.com/"),
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
